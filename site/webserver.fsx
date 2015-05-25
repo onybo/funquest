6aaee8432989e0e4f19b1612722cc0b9eff96fee
on <olavnybo@gmail.com>
2015-05-25 20:38:41 +0200

    added some packages and trying to read environment variable

--- site/webserver.fsx
+++ site/webserver.fsx
@@ -21,4 +21,5 @@ startWebServer serverConfig
         ("Hello World! It's Suave.io on Azure Websites.<br /><ul>" + 
           "<li>Sample git Repo: <a href='https://github.com/shanselman/suavebootstrapper'>https://github.com/shanselman/suavebootstrapper</a></li>" +
           "<li>Intro blog post: <a href='http://www.hanselman.com/blog/RunningSuaveioAndFWithFAKEInAzureWebAppsWithGitAndTheDeployButton.aspx'>http://www.hanselman.com/blog/RunningSuaveioAndFWithFAKEInAzureWebAppsWithGitAndTheDeployButton.aspx</a></li>" +
+          "<li>Setting is: " + Environment.GetEnvironmentVariable("APPSETTING_USER_NAME") + 
           "</ul>"))
